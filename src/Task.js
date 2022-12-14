c3403645e15ddd6321ba07f572771d30387d589e
Betha Rianti Nadapdap <betharianti02@gmail.com>
2022-12-14 22:15:10 +0700

    change url in task.js

--- src/Task.js
+++ src/Task.js
@@ -1,4 +1,4 @@
-const url = "YOURDEPLOYURL";
+const url = "https://betharianti-deployment-assignment.netlify.app/";
 const studentName = "Betha Rianti Br Nadapdap";
 const studentId = "FE2655725";
 
